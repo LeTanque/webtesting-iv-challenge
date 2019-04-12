5fce147e2e488f05ed257673c9cd76067e3efe5c
Frank Martinez <frankie.martinez@gmail.com>
2019-04-11 23:53:06 -0700

    more tests on remove endpoint

--- routes/router.spec.js
+++ routes/router.spec.js
@@ -8,7 +8,7 @@ describe('users model', () => {
     });
 
     describe('insert()', () => {
-        it('should insert the provided users', async () => {
+        it('should insert the provided users, check length', async () => {
             await Users.insert({ username: 'tank' });
             await Users.insert({ username: 'glaston' });
             await Users.insert({ username: 'theory' });
@@ -16,7 +16,7 @@ describe('users model', () => {
             expect(users).toHaveLength(3);
         });
 
-        it('should insert the provided users', async () => {
+        it('should insert the provided user, check existence', async () => {
             let users = await Users.insert({ username: 'tank' });
             expect(users.username).toBe('tank');
 
@@ -26,13 +26,34 @@ describe('users model', () => {
     });
 
     describe('remove()', () => {
-        it('should remove the inserted user', async () => {
+        it('should remove the inserted users, check length is zero', async () => {
             await Users.insert({ username: 'tank' });
             await Users.insert({ username: 'tonfrat' });
             await Users.remove(2);
             await Users.remove(1);
             const users = await db('users');
             expect(users).toHaveLength(0);
+        });
+        it('should remove the inserted user, check length is one', async () => {
+            await Users.insert({ username: 'tank' });
+            await Users.insert({ username: 'yonder' });
+            await Users.remove(2);
+            const users = await db('users');
+            expect(users).toHaveLength(1);
+        });
+        it('should remove id 2, check username at index 0', async () => {
+            await Users.insert({ username: 'tank' });
+            await Users.insert({ username: 'yonder' });
+            await Users.remove(2);
+            const users = await db('users');
+            expect(users[0].username).toBe('tank');
+        });
+        it('should remove id 1, check username at index 0 ', async () => {
+            await Users.insert({ username: 'tank' });
+            await Users.insert({ username: 'yonder' });
+            await Users.remove(1);
+            const users = await db('users');
+            expect(users[0].username).toBe('yonder');
         });
     });
 });
